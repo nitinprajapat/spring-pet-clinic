b40cd681f710071402286e3a3916aa669c856967
flyingnitin <nitin03@nagarro.com>
2021-04-20 22:15:14 +0530

    adding POJO's. Closes #2.

---
+++ src/main/java/flying/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package flying.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/flying/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package flying.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
