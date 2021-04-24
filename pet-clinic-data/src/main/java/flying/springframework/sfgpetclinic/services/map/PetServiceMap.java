d3d6734e9e3cbba09716d1b2dac4b604bb2d0aa7
flyingnitin <nitin03@nagarro.com>
2021-04-24 21:24:13 +0530

    implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/flying/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package flying.springframework.sfgpetclinic.services.map;
+
+import flying.springframework.sfgpetclinic.model.Pet;
+import flying.springframework.sfgpetclinic.services.CrudService;
+import flying.springframework.sfgpetclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
