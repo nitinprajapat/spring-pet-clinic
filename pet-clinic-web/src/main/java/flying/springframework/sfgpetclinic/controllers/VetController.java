c7472849001c68c04e0473209c638c03ac3f5d7a
flyingnitin <nitin03@nagarro.com>
2021-04-24 22:22:27 +0530

    Added vets page and controller. closes #12

---
+++ pet-clinic-web/src/main/java/flying/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package flying.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
